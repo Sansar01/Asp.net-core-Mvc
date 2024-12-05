33e59b68ac72ba5cc0e89e71c9674c93b895850d
Sansar Tiwari <st9889477@gmail.com>
2024-12-05 21:55:34 +0530

    update of data pending

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -39,10 +39,39 @@ namespace Asp.net_core_Mvc.Controllers
             if (ModelState.IsValid)
             {
                 await studentDb.Students.AddAsync(std);
-                await  studentDb.SaveChangesAsync();
+                await studentDb.SaveChangesAsync();
                 return RedirectToAction("Index", "Home");
             }
             return View();
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var studentDetail = await studentDb.Students.Where(item => item.Id == id).FirstAsync();
+
+            return View(studentDetail);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var updateData = await studentDb.Students.Where(item => item.Id == id).FirstAsync();
+
+            return View(updateData);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student std)
+        {
+            var findData = await studentDb.Students.Where(item => item.Id == std.Id).FirstAsync();
+
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            await studentDb.Students.Where(item => item.Id == id).ExecuteDeleteAsync();
+            await studentDb.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
         }
 
         public IActionResult Privacy()
